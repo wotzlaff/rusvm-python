f4438b19fc08108438bff049aea638dd142b3379
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2023-11-06 17:08:25 +0100

    add interface for newton

--- src/lib.rs
+++ src/lib.rs
@@ -111,6 +111,20 @@ fn extract_params_smo(params_dict: Option<&PyDict>) -> PyResult<rusvm::smo::Para
     Ok(params)
 }
 
+fn extract_params_newton(params_dict: Option<&PyDict>) -> PyResult<rusvm::newton::Params> {
+    check_params(
+        params_dict,
+        vec!["tol", "max_steps", "verbose", "time_limit"].as_slice(),
+    )?;
+
+    let mut params = rusvm::newton::Params::new();
+    params.tol = extract::<f64>(params_dict, "tol")?.unwrap_or(params.tol);
+    params.max_steps = extract::<usize>(params_dict, "max_steps")?.unwrap_or(params.max_steps);
+    params.verbose = extract::<usize>(params_dict, "verbose")?.unwrap_or(params.verbose);
+    params.time_limit = extract::<f64>(params_dict, "time_limit")?.unwrap_or(params.time_limit);
+    Ok(params)
+}
+
 fn prepare_problem<'a>(
     kind: &str,
     y: &'a &[f64],
@@ -230,5 +244,51 @@ fn pyrusvm<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
         let py_result = status_to_dict(&result, py);
         Ok(py_result)
     }
+
+    #[pyfn(m)]
+    #[pyo3(signature = (x, y, kind = "classification", params_problem = None, params_newton = None, cache_size = 0, callback = None))]
+    fn solve_newton<'py>(
+        py: Python<'py>,
+        x: PyReadonlyArray2<'py, f64>,
+        y: PyReadonlyArray1<'py, f64>,
+        kind: &str,
+        params_problem: Option<&PyDict>,
+        params_newton: Option<&PyDict>,
+        cache_size: usize,
+        callback: Option<&PyAny>,
+    ) -> PyResult<PyObject> {
+        // get SMO parameter
+        let params_newton = extract_params_newton(params_newton)?;
+
+        // prepare problem
+        let y = y.as_slice()?;
+        let problem = prepare_problem(kind, &y, params_problem)?;
+
+        // prepare kernel
+        let mut base = Box::new(rusvm::kernel::GaussianKernel::new(1.0, x.as_array()));
+        let mut kernel: Box<dyn rusvm::kernel::Kernel> = {
+            if cache_size > 0 {
+                Box::new(rusvm::kernel::CachedKernel::from(base.as_mut(), cache_size))
+            } else {
+                base
+            }
+        };
+
+        // prepare callback
+        let callback = prepare_callback(py, callback)?;
+
+        // solve problem
+        let result = rusvm::newton::solve(
+            problem.as_ref(),
+            kernel.as_mut(),
+            &params_newton,
+            callback.as_deref(),
+        );
+
+        // return results
+        let py_result = status_to_dict(&result, py);
+        Ok(py_result)
+    }
+
     Ok(())
 }
